fbb58ae39e390cda8b225e6f698c81a594c4fc1e
grkmuysl <gorkmuysal@gmail.com>
2026-08-06 11:52:22 +0300

    Create Entities
    Created Task and User entities

---
+++ src/main/java/com/gorkemuysal/entity/Task.java
@@ -0,0 +1,39 @@
+package com.gorkemuysal.entity;
+
+import java.util.Date;
+
+import com.gorkemuysal.enums.TaskStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "task")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String title;
+	
+	private String description;
+	
+	@Enumerated(EnumType.STRING)
+	private TaskStatus status;
+	
+	private Date dueDate;
+	
+	@ManyToOne
+	private User user;
+}
