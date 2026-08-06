97507effb257766c62d7eeaa9a97e5551144bf7e
grkmuysl <gorkmuysal@gmail.com>
2026-08-06 11:22:09 +0300

    Initial commit

---
+++ src/main/java/com/gorkemuysal/multi_user_todo/MultiUserTodoApplication.java
@@ -0,0 +1,13 @@
+package com.gorkemuysal.multi_user_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiUserTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiUserTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gorkemuysal/multi_user_todo/MultiUserTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorkemuysal.multi_user_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiUserTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
