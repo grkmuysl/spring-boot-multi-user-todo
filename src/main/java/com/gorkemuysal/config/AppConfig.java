79a7fb39da643e970377f54a55857dd5981c51e0
grkmuysl <gorkmuysal@gmail.com>
2026-08-13 12:49:15 +0300

    Security Config Class
    1- I deleted WebSecurityConfig class. Instead of I created new class that name is SecurityConfig
    2- I replaced passwordEncoder function.

--- src/main/java/com/gorkemuysal/config/AppConfig.java
+++ src/main/java/com/gorkemuysal/config/AppConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.gorkemuysal.entity.UserDetailsImpl;
 import com.gorkemuysal.repository.UserRepository;
@@ -45,9 +47,14 @@ public class AppConfig {
 	@Bean
 	public AuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService());
-		authenticationProvider.setPasswordEncoder(securityConfig.passwordEncoder());
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
 
 		return authenticationProvider;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
