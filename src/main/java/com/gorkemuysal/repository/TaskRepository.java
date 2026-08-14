768b120687c1eb08b08f320988cbd19ff6d52fb9
grkmuysl <gorkmuysal@gmail.com>
2026-08-14 12:43:22 +0300

    Update TaskRepository
    I added 3 new method to find Task according to filters

--- src/main/java/com/gorkemuysal/repository/TaskRepository.java
+++ src/main/java/com/gorkemuysal/repository/TaskRepository.java
@@ -1,11 +1,21 @@
 package com.gorkemuysal.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.gorkemuysal.entity.Task;
+import com.gorkemuysal.enums.TaskStatus;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>{
 
+	List<Task> findByUserId(Long userId);
+	
+	Optional<Task> findByIdAndUserId(Long id, Long userId);
+	
+	List<Task> findByUserIdAndStatus(Long userId, TaskStatus status);
+	
 }
