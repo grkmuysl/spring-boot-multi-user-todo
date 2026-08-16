98aab896b67d3e1215c4cba255340866ff1004e5
grkmuysl <gorkmuysal@gmail.com>
2026-08-16 15:44:54 +0300

    Implement CustomAuthenticationEntryPoint
    I implemented new class name is CustomAuthenticationEntryPoint. Thanks to this class I can catch auth error before controller checks

--- src/main/java/com/gorkemuysal/config/SecurityConfig.java
+++ src/main/java/com/gorkemuysal/config/SecurityConfig.java
@@ -7,9 +7,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.gorkemuysal.exception.CustomAuthenticationEntryPoint;
 import com.gorkemuysal.jwt.JwtAuthenticationFilter;
 
 @Configuration
@@ -18,32 +20,32 @@ public class SecurityConfig {
 
 	public static final String AUTHENTICATE = "/auth";
 	public static final String REGISTER = "/register";
-	
+
+	public final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
 	private final AuthenticationProvider authenticationProvider;
-	
+
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-	SecurityConfig(AuthenticationProvider authenticationProvider , JwtAuthenticationFilter jwtAuthenticationFilter) {
+	SecurityConfig(AuthenticationProvider authenticationProvider, JwtAuthenticationFilter jwtAuthenticationFilter,
+			CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
 		this.authenticationProvider = authenticationProvider;
 		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+		this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
 	}
 
-	
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http)  throws Exception {
-		
-		  http
-          .csrf(csrf -> csrf.disable())
-          .authorizeHttpRequests(auth -> auth
-              .requestMatchers(AUTHENTICATE , REGISTER).permitAll()
-              .anyRequest().authenticated()
-          )
-          .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-          .authenticationProvider(authenticationProvider)
-          .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-          .httpBasic(Customizer.withDefaults());
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-      return http.build();
-		
+		http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(
+						auth -> auth.requestMatchers(AUTHENTICATE, REGISTER).permitAll().anyRequest().authenticated())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authenticationProvider(authenticationProvider)
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+				.exceptionHandling(exceptionHandling -> exceptionHandling
+						.authenticationEntryPoint(customAuthenticationEntryPoint));
+		return http.build();
+
 	}
 }
