3671109c86aa8c3d6bf58a48cbf8f662075e1a5c
grkmuysl <gorkmuysal@gmail.com>
2026-08-17 16:02:01 +0300

    Implement 2 tests
    I implemented 2 test for create task process.

--- src/test/java/com/gorkemuysal/multi_user_todo/MultiUserTodoApplicationTests.java
+++ src/test/java/com/gorkemuysal/multi_user_todo/MultiUserTodoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.gorkemuysal.multi_user_todo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.gorkemuysal.dto.TaskResponse;
+import com.gorkemuysal.service.ITaskService;
+
+@SpringBootTest(classes = { MultiUserTodoApplication.class })
 class MultiUserTodoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private ITaskService taskService;
 
+	@Test
+	public void testGetTaskById() {
+		TaskResponse response = taskService.getTaskById(2L, 15L);
+
+		if (response != null) {
+			System.out.println("TASK NAME IS:" + response.getTitle());
+		}
+	}
 }
